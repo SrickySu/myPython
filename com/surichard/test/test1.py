003d0ca60c0199c3454c94867c27ac5083358ec9
su.qingjia <su.qingjia@yuexiu-finance.com>
2019-04-18 16:56:35 +0800

    update a test file

--- com/surichard/test/test1.py
+++ com/surichard/test/test1.py
@@ -1,9 +1,10 @@
-#coding=utf-8
+# coding=utf-8
 '''
 Created on 2019年1月1日
 
 @author: 74518
 '''
+import re, mysql.connector
 class Person(object):
     def __init__(self, name, gender, birth, job):
         self.name = name
@@ -15,3 +16,13 @@ xiaoming = Person('Xiao Ming', 'Male', '1990-1-1', job='Student')
 
 print xiaoming.name
 print xiaoming.job
+
+conn = mysql.connector.Connect(host='', port='3306', user='root', password='', database='mysql', charset='utf8')
+cursor = conn.cursor()
+cursor.execute('select * from user limit 2')
+results = cursor.fetchall()
+for re in results:
+    print re
+conn.close()
+
+
